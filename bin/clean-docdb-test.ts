867fb7ff579d2dfca6386e7f9f9f2cf008557231
Ryan Owens <rowens@architectnow.net>
2021-04-15 16:10:18 -0500

    initial commit

--- bin/clean-docdb-test.ts
+++ bin/clean-docdb-test.ts
@@ -18,4 +18,8 @@ new CleanDocdbTestStack(app, 'CleanDocdbTestStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
 });
--- lib/clean-docdb-test-stack.ts
+++ lib/clean-docdb-test-stack.ts
@@ -1,9 +1,27 @@
 import * as cdk from '@aws-cdk/core';
+import { Vpc, InstanceType, InstanceClass, InstanceSize, SubnetType } from '@aws-cdk/aws-ec2';
+import { DatabaseCluster } from '@aws-cdk/aws-docdb';
 
 export class CleanDocdbTestStack extends cdk.Stack {
+  username: "testUser";
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Same VPC as other code
+    const vpc = Vpc.fromLookup(this, 'existing-vpc', {
+      vpcId: "vpc-044f1f7fb9eb44197"
+    });
+
+    const databaseCluster = new DatabaseCluster(this, 'todos-database', {
+      masterUser: {
+          username: this.username, // NOTE: 'admin' is reserved by DocumentDB
+      },
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM), // T3 instead of M3
+      vpcSubnets: {
+          subnetType: SubnetType.PRIVATE
+      },
+      vpc: vpc,
+      dbClusterName: "test-database-cluster",
+  });
   }
 }
